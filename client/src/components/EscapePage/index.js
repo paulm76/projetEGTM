238d4bb6da1140038ff52cb34ee632bc428bbe2b
Paul <paul.michon@devinci.fr>
2017-12-21 16:32:16 +0100

    ajout script, modif escapePage, et +

--- client/src/components/EscapePage/index.js
+++ client/src/components/EscapePage/index.js
@@ -44,40 +44,38 @@ class TeamPage extends Component {
 	    return (
 	      <div style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
 	      	<Grid centered columns={2} stackable>
-        	  <Grid.Column computer='12'>
-      			<Segment vertical style={{ display:'flex', justifyContent: 'space-between' }}>
-      		  	  <div>
+        	  <Grid.Column computer='8'>
+        			<Segment vertical>
         	  		<h1>{escape[0].Nom}</h1>
         	  		<Icon name='marker' />{escape[0].Adresse}, {escape[0].Code_postal} {escape[0].Ville}
-      		  	  </div>
-      		  	  <div style={{ position: 'right' }}>
-			    	<Image src={escape[0].Photo}/>
-      		  	  </div>
-      			</Segment>
-      		
-      			<Segment vertical>
-      		  	  <h2>Liste des rooms</h2>
-      			  <List style={{ display: 'flex' }}>
-			  		{rooms.map(room => 
-			  		  <Link to={`/room?room=${room.Nom.replace(' ','_')}`}><List.Item key={room.Nom} style={{ marginRight: '20px', padding: '0px', backgroundImage: "url('" + room.Photo + "')", backgroundSize: '270px 120px', backgroundRepeat: 'no-repeat', color:'white', width: '270px', height: '120px' }}> 
-			  		  	<h3>{room.Nom}</h3>
-			  		  	<p>{room.Theme}<br/>
-			  		  	  {room.Difficulte}<br/>
-			  		  	  De {room.Nb_places_min} à {room.Nb_places_max} joueurs
-			  		  	</p> 
-			  		  </List.Item></Link>)}
-      			  </List>
-      			</Segment>
-      			<h2>Localisation </h2>
-      			<GoogleMapMarker
-        	  	  isMarkerShown
-        	  	  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBYzbUIeVSGKMU0-KCJYWD1_X4qC_OsrI&v=3.exp&libraries=geometry,drawing,places"
-        	  	  loadingElement={<div style={{ height: `100%` }} />}
-        	  	  containerElement={<div style={{ height: `400px` }} />}
-        	  	  mapElement={<div style={{ height: `100%` }} />}
-        	  	  position = {{lat: escape[0].Latitude, lng: escape[0].Longitude}}
-      			/>
-          	  </Grid.Column>
+        			</Segment>
+        		
+        			<Segment vertical>
+        		  	  <h2>Liste des rooms</h2>
+        			  <List style={{ display: 'flex' }}>
+    		  		{rooms.map(room => 
+    		  		  <Link to={`/room?room=${room.Nom.replace(' ','_')}`}><List.Item key={room.Nom} style={{ marginRight: '20px', padding: '0px', backgroundImage: "url('" + room.Photo + "')", backgroundSize: '270px 120px', backgroundRepeat: 'no-repeat', color:'white', width: '270px', height: '120px' }}> 
+    		  		  	<h3>{room.Nom}</h3>
+    		  		  	<p>{room.Theme}<br/>
+    		  		  	  {room.Difficulte}<br/>
+    		  		  	  De {room.Nb_places_min} à {room.Nb_places_max} joueurs
+    		  		  	</p> 
+    		  		  </List.Item></Link>)}
+        			  </List>
+        			</Segment>
+        			<h2>Localisation </h2>
+        			<GoogleMapMarker
+          	  	  isMarkerShown
+          	  	  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBYzbUIeVSGKMU0-KCJYWD1_X4qC_OsrI&v=3.exp&libraries=geometry,drawing,places"
+          	  	  loadingElement={<div style={{ height: `100%` }} />}
+          	  	  containerElement={<div style={{ height: `400px` }} />}
+          	  	  mapElement={<div style={{ height: `100%` }} />}
+          	  	  position = {{lat: escape[0].Latitude, lng: escape[0].Longitude}}
+        			/>
+      	  </Grid.Column>
+          <Grid.Column computer='5'>
+            <Image src={escape[0].Photo}/>
+          </Grid.Column>
       		</Grid>
 	      </div>
 	    ); 
