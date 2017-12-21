1e2b991b8437c928a701f0502e80cdec6a57d484
Jean-Paul <champy_jean-paul@live.fr>
2017-12-21 14:15:28 +0100

    encrypt password using bcrypt

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+
+var cryptPassword = function(password, callback) {
+   bcrypt.genSalt(10, function(err, salt) {
+    if (err)
+      return callback(err);
+
+    bcrypt.hash(password, salt, function(err, hash) {
+      return callback(err, hash);
+    });
+  });
+};
+/*
+var comparePassword = function(plainPass, hashword, callback) {
+   bcrypt.compare(plainPass, hashword, function(err, isPasswordMatch) {
+       return err == null ?
+           callback(null, isPasswordMatch) :
+           callback(err);
+   });
+};*/
+
 
 router.post('/',function(req, res) {
   console.log("testpost");
@@ -12,6 +33,13 @@ router.post('/',function(req, res) {
   console.log(email);
   password=req.body.password;
   console.log(password);
+  var hashedPassword="nothashedyet";
+  cryptPassword(password,function(err,hash){
+
+    hashedPassword=hash;
+    console.log("err:"+err);
+    console.log("hash:"+hashedPassword);
+  });
 
   //console.log(res.text());
   //connection.query('Insert into joueur_equipe(id_joueur,id_equipe) values (1,1)');//insert
