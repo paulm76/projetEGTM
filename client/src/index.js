5899191b4a33cd6c0fe93cf654f8fef8e6296114
Jean-Paul <champy_jean-paul@live.fr>
2018-01-22 14:49:42 +0100

    src index merge

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
+import  'react-password-strength/dist/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
